30d69c0beaf80488c42ac8eeb0122150607a3480
fajaramaulana <fajaragusmaulana@gmail.com>
2024-01-17 12:08:58 +0700

    Add authHelper.js for password hashing and verification

---
+++ src/utils/authHelper.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt')
+let saltRounds = process.env.SALT || 10 // Number of salt rounds, higher is more secure but slower
+
+// Hashing a password
+const hashPassword = async (password) => {
+  try {
+    saltRounds = parseInt(saltRounds, 10)
+    console.log(`saltRounds: ${saltRounds}`)
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    console.log(`hashPassword: ${hashedPassword}`)
+    return hashedPassword
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error hashing password')
+  }
+}
+
+// Verifying a password
+const verifyPassword = async (password, hashedPassword) => {
+  try {
+    const match = await bcrypt.compare(password, hashedPassword)
+    return match
+  } catch (error) {
+    throw new Error('Error verifying password')
+  }
+}
+
+module.exports = {
+  hashPassword,
+  verifyPassword
+}
