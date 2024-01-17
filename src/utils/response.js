a02bfadb2545e915a12afb0a28e99368a980147d
fajaramaulana <fajaragusmaulana@gmail.com>
2024-01-17 15:49:27 +0700

    Add response utility functions

---
+++ src/utils/response.js
@@ -0,0 +1,45 @@
+// responseApi.js
+
+const response = {
+  success: (data = null, message = 'Request succeeded') => ({
+    success: true,
+    data,
+    message
+  }),
+
+  error: (message = 'Internal Server Error', code = 500, errors = []) => ({
+    code,
+    success: false,
+    message,
+    error: errors
+  }),
+
+  detail: (data = null, message = 'Request succeeded') => ({
+    success: true,
+    type: 'detail',
+    data,
+    message
+  }),
+
+  pagination: (items = [], totalCount = 0, limit = 10, offset = 0, search = []) => ({
+    success: true,
+    meta: {
+      totalCount,
+      limit,
+      offset,
+      search
+    },
+    type: 'pagination',
+    data: items,
+    message: 'Request succeeded'
+  }),
+
+  nonPagination: (data = [], message = 'Request succeeded') => ({
+    success: true,
+    type: 'nonPagination',
+    data,
+    message
+  })
+}
+
+module.exports = response
